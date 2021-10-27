cdb626de71e5432461995fc59d12a4fbaec69804
Stijn Uytterhoeven <stijnuytterhoeven@Stijns-MacBook-Pro.local>
2021-10-27 13:20:14 +0200

    recombination implemented

--- r0123456.py
+++ r0123456.py
@@ -22,15 +22,19 @@ class r0123456:
 		# Your code here.
 		popSize = 40
 		population = self.initPopulation(popSize, range(len(distanceMatrix[0])))
-		for gene in population:
-			print(self.getFitness(gene, distanceMatrix))
 		
 		nbIter = 0
-		while( nbIter < 10000 ):
+		
+	
+		while( nbIter < 10000):
 
-			selectedPop = self.selection(population)
-			recombinatedPop = self.recombination(selectedPop)
+			selectedPop = self.selection(population) 
+			
+			# parent pairs are given as input and all their childs are given as output
+			recombinatedPop = self.recombination(selectedPop) 
+
 			mutatedPop = self.mutation(recombinatedPop)
+			
 			finalPop = self.elimination(mutatedPop)
 
 			allObjectives = [self.getObjective(route, distanceMatrix) for route in finalPop]
@@ -58,9 +62,66 @@ class r0123456:
 	def elimination(population):
 		return None
 
-	def recombination(population):
-		return None
+# returns list of children after recombination of parent pairs
+	def recombination(self, population):
+		newPop = []
+		for pair in population:
+			newPop.append(self.OX(pair))
+			newPop.append(self.POS(pair))
+		return newPop
 
+# recombination operators
+	def POS(self, parents):
+		parent1 = parents[0]
+		parent2 = parents[1]
+		positions = []
+		child = list(-1 for _ in range(len(parent1)))
+
+		for _ in range(random.randint(0,len(parent1) - 1)):
+			position = random.randint(0,len(parent1) - 1)
+			if position not in positions:
+				positions.append(position)
+
+		for i in positions:
+			child[i] = parent1[i]
+		
+		j = 0
+		for i in range(len(parent2)):
+			if child[i] == -1:
+				while parent2[j] in child:
+					j += 1
+				child[i] = parent2[j]
+
+		return child
+
+	def OX(self, parents):
+		parent1 = parents[0]
+		parent2 = parents[1]
+		child = list(-1 for _ in range(len(parent1)))
+
+		# partition 1 will take place after the position of partition1
+		# partition 2 will take place before the position of partition2
+		partition1 = random.randint(1, len(parent1) - 1)
+		partition2 = random.randint(1, len(parent1) - 1)
+
+		lowerpartition = min(partition1,partition2)
+		higherpartition = max(partition1, partition2)
+
+		# middle part of parent1 is copied
+		child[lowerpartition: higherpartition] = parent1[lowerpartition: higherpartition]
+		parent2 = parent2[higherpartition:] + parent2[:higherpartition]
+
+		j = higherpartition
+		for i in parent2:
+			if i not in child:
+				child[j] = i
+				j += 1
+				if j >= len(child):
+					j = 0
+
+		return child
+	
+	# returns list of lists with each 2 parents 
 	def selection(population):
 		return None
 
