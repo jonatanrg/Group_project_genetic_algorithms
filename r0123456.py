0dc4726f5af65e161f3e24ed0e76bbad98dea6a5
Jonatan Risberg <jonatanrisberg@Jonatans-MacBook-Pro.local>
2021-10-27 12:07:36 +0200

    added implementation of elimination operator and minor fixes.

--- r0123456.py
+++ r0123456.py
@@ -1,6 +1,5 @@
-import math 
+import math
 import random
-from statistics import mean
 import numpy as np
 
 import Reporter
@@ -9,89 +8,108 @@ import Reporter
 # Modify the class name to match your student number.
 class r0123456:
 
-	def __init__(self):
-		self.reporter = Reporter.Reporter(self.__class__.__name__)
+    def __init__(self):
+        self.reporter = Reporter.Reporter(self.__class__.__name__)
 
-	# The evolutionary algorithm's main loop
-	def optimize(self, filename):
-		# Read distance matrix from file.		
-		file = open(filename)
-		distanceMatrix = np.loadtxt(file, delimiter=",")
-		file.close()
-		
-		# Your code here.
-		popSize = 40
-		population = self.initPopulation(popSize, range(len(distanceMatrix[0])))
-		for gene in population:
-			print(self.getFitness(gene, distanceMatrix))
-		
-		nbIter = 0
-		while( nbIter < 10000 ):
+    # The evolutionary algorithm's main loop
+    def optimize(self, filename):
+        # Read distance matrix from file.
+        file = open(filename)
+        distanceMatrix = np.loadtxt(file, delimiter=",")
+        file.close()
 
-			selectedPop = self.selection(population)
-			recombinatedPop = self.recombination(selectedPop)
-			mutatedPop = self.mutation(recombinatedPop)
-			finalPop = self.elimination(mutatedPop)
+        # Your code here.
+        popSize = 40
+        population = self.initPopulation(popSize, range(len(distanceMatrix[0])))
+        for gene in population:
+            print(self.getObjective(gene, distanceMatrix))
 
-			allObjectives = [self.getObjective(route, distanceMatrix) for route in finalPop]
-			
-			bestObjective = min(allObjectives)
-			meanObjective = mean(allObjectives)
-			bestSolution = finalPop[allObjectives.index(bestObjective)]
-			
-			# Call the reporter with:
-			#  - the mean objective function value of the population
-			#  - the best objective function value of the population
-			#  - a 1D numpy array in the cycle notation containing the best solution 
-			#    with city numbering starting from 0
-			timeLeft = self.reporter.report(meanObjective, bestObjective, bestSolution)
-			if timeLeft < 0:
-				break
+        nbIter = 0
+        while (nbIter < 10000):
 
-			nbIter += 1
-		# Your code here.
-		return 0
-	
-	def mutation(population):
-		return None
-	
-	def elimination(population):
-		return None
+            selectedPop = self.selection(population)
+            recombinatedPop = self.recombination(selectedPop)
+            mutatedPop = self.mutation(recombinatedPop)
+            finalPop = self.elimination(mutatedPop)
 
-	def recombination(population):
-		return None
+            allObjectives = [self.getObjective(route, distanceMatrix) for route in finalPop]
 
-	def selection(population):
-		return None
+            bestObjective = min(allObjectives)
+            meanObjective = np.mean(allObjectives)
+            bestSolution = finalPop[allObjectives.index(bestObjective)]
 
-	def generateRoute(self, cityList):
-		route = random.sample(cityList, len(cityList))
-		return route
-	
-	def initPopulation(self, size, cityList):
-		population = list()
+            # Call the reporter with:
+            #  - the mean objective function value of the population
+            #  - the best objective function value of the population
+            #  - a 1D numpy array in the cycle notation containing the best solution
+            #    with city numbering starting from 0
+            timeLeft = self.reporter.report(meanObjective, bestObjective, bestSolution)
+            if timeLeft < 0:
+                break
 
-		for _ in range(size):
-			population.append(self.generateRoute(cityList))
-		return population
-	
-	def getObjective(self, route, distanceMatrix):
-		totalLength = 0
-		for index in range(len(route)):
-			city1 = route[index]
-			try:
-				city2 = route[index + 1]
-			except IndexError:
-				city2 = route[0]
-			
-			totalLength += self.getLength(city1, city2, distanceMatrix)
+            nbIter += 1
+        # Your code here.
+        return 0
 
-		return totalLength
-	
-	def getLength(self, city1, city2, distanceMatrix):
-		return distanceMatrix[city1][city2]
+    def mutation(population):
+        return None
+
+    def elimination(self, population, distanceMatrix):
+        # fitness-based elimination
+        pop_fitness = np.zeros(len(population))
+        for i in range(pop_fitness):
+            pop_fitness[i] = self.getObjective(population[i], distanceMatrix)
+
+        mean_fitness = np.mean(pop_fitness)
+
+        # Select size of subset population
+        percentage = 0.6
+        subset_size = int(len(population)*percentage)
+
+        for _ in range(subset_size):
+            index = random.randint(0, len(population))
+            fitness = self.getObjective(population[index], distanceMatrix)
+
+            # Eliminate elements with below average fitness
+            if fitness < mean_fitness:
+                del population[index]
+
+        return population
+
+    def recombination(population):
+        return None
+
+    def selection(population):
+        return None
+
+    def generateRoute(self, cityList):
+        route = random.sample(cityList, len(cityList))
+        return route
+
+    def initPopulation(self, size, cityList):
+        population = list()
+
+        for _ in range(size):
+            population.append(self.generateRoute(cityList))
+        return population
+
+    def getObjective(self, route, distanceMatrix):
+        totalLength = 0
+        for index in range(len(route)):
+            city1 = route[index]
+            try:
+                city2 = route[index + 1]
+            except IndexError:
+                city2 = route[0]
+
+            totalLength += self.getLength(city1, city2, distanceMatrix)
+
+        return totalLength
+
+    def getLength(self, city1, city2, distanceMatrix):
+        return distanceMatrix[city1][city2]
 
 
 if __name__ == '__main__':
-	test = r0123456()
-	test.optimize('tour29.csv')
+    test = r0123456()
+    test.optimize('tour29.csv')
